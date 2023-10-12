2fbb5ade69ecb5fc2a2afc1b9ea4b7b99010f974
Arif Budiman Arrosyid <arifbudimanarrosyid@gmail.com>
2023-10-13 01:12:44 +0700

    add transition to dropdown

--- resources/views/components/nav-dropdown.blade.php
+++ resources/views/components/nav-dropdown.blade.php
@@ -23,7 +23,11 @@ just nav-link or responsive nav-link --}}
         </span>
     </button>
 
-    <div class="flex flex-col gap-1 ml-3 lg:ml-4 lg:relative" style="display: none;" x-show="open">
+    <div class="flex flex-col gap-1 ml-3 lg:ml-4 lg:relative" style="display: none;" x-show="open"
+        x-transition:enter="transition ease-out duration-200" x-transition:enter-start="transform opacity-0 scale-95"
+        x-transition:enter-end="transform opacity-100 scale-100" x-transition:leave="transition ease-in duration-75"
+        x-transition:leave-start="transform opacity-100 scale-100"
+        x-transition:leave-end="transform opacity-0 scale-95">
         {{ $slot }}
     </div>
 </div>
--- resources/views/layouts/main-navigation.blade.php
+++ resources/views/layouts/main-navigation.blade.php
@@ -197,7 +197,11 @@
 
     <!-- Mobile menu, show/hide based on menu state. -->
     <div class="hidden border-t border-gray-100 sm:hidden dark:border-gray-700" id="mobile-menu"
-        :class="{ 'block': open, 'hidden': !open }">
+        :class="{ 'block': open, 'hidden': !open }" x-show='open'
+        x-transition:enter="transition ease-out duration-200" x-transition:enter-start="transform opacity-0 scale-95"
+        x-transition:enter-end="transform opacity-100 scale-100" x-transition:leave="transition ease-in duration-75"
+        x-transition:leave-start="transform opacity-100 scale-100"
+        x-transition:leave-end="transform opacity-0 scale-95">
         <div class="px-2 py-2 space-y-1">
             <x-responsive-nav-link wire:navigate href="{{ route('home') }}" :active="request()->routeIs('home')">
                 {{ __('Home') }}
