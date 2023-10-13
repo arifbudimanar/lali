64703cf22222ae76f02e41404e2cf9d490c057e8
Arif Budiman Arrosyid <arifbudimanarrosyid@gmail.com>
2023-10-14 05:19:01 +0700

    add number loop

--- resources/views/livewire/admin/users/index.blade.php
+++ resources/views/livewire/admin/users/index.blade.php
@@ -59,6 +59,15 @@
                     <x-table>
                         <x-table-head>
                             <x-table-head-th>
+                                {{ __('No') }}
+                                <a href="#"><svg class="w-3 h-3 ml-1.5" aria-hidden="true"
+                                        xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
+                                        <path
+                                            d="M8.574 11.024h6.852a2.075 2.075 0 0 0 1.847-1.086 1.9 1.9 0 0 0-.11-1.986L13.736 2.9a2.122 2.122 0 0 0-3.472 0L6.837 7.952a1.9 1.9 0 0 0-.11 1.986 2.074 2.074 0 0 0 1.847 1.086Zm6.852 1.952H8.574a2.072 2.072 0 0 0-1.847 1.087 1.9 1.9 0 0 0 .11 1.985l3.426 5.05a2.123 2.123 0 0 0 3.472 0l3.427-5.05a1.9 1.9 0 0 0 .11-1.985 2.074 2.074 0 0 0-1.846-1.087Z" />
+                                    </svg>
+                                </a>
+                            </x-table-head-th>
+                            <x-table-head-th>
                                 {{ __('Id') }}
                                 <a href="#"><svg class="w-3 h-3 ml-1.5" aria-hidden="true"
                                         xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
@@ -111,6 +120,9 @@
                         <x-table-body>
                             @forelse ($users as $user)
                                 <x-table-body-tr wire:key="{{ $user->id }}">
+                                    <x-table-body-th>
+                                        {{ $loop->iteration + $users->firstItem() - 1 }}
+                                    </x-table-body-th>
                                     <x-table-body-th>
                                         {{ $user->id }}
                                     </x-table-body-th>
