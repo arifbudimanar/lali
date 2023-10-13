b2f46cb59c8ba09bfca9efa7c250caf2b27aac1d
Arif Budiman Arrosyid <arifbudimanarrosyid@gmail.com>
2023-10-14 04:58:41 +0700

    wip

--- resources/views/components/action-message.blade.php
+++ resources/views/components/action-message.blade.php
@@ -3,7 +3,7 @@
 <div x-data="{ shown: false, timeout: null }" x-init="@this.on('{{ $on }}', () => {
     clearTimeout(timeout);
     shown = true;
-    timeout = setTimeout(() => { shown = false }, 10000);
+    timeout = setTimeout(() => { shown = false }, 2000);
 })" x-show.transition.out.opacity.duration.1500ms="shown"
     x-transition:leave.opacity.duration.1500ms style="display: none;"
     {{ $attributes->merge(['class' => 'text-sm text-gray-600 dark:text-gray-400']) }}>
