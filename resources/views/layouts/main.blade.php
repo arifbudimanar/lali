5eb707747fc56c3b1d1019b68fd35deca856585d
Arif Budiman Arrosyid <arifbudimanarrosyid@gmail.com>
2023-10-15 02:00:59 +0700

    wip

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -5,6 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link rel="Lali" href="{{ asset('favicon.ico') }}" type="image/x-icon">
 
     <title>
         {{ config('app.name', 'Laravel') }}
