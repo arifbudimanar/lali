c7b8f412803ee13af898ceb50446025a278deabb
Arif Budiman Arrosyid <arifbudimanarrosyid@gmail.com>
2023-10-17 01:07:06 +0700

    add logo to error page

--- resources/views/errors/minimal.blade.php
+++ resources/views/errors/minimal.blade.php
@@ -16,9 +16,11 @@
 <body class="antialiased">
     <div
         class="relative flex items-center justify-center min-h-screen bg-zinc-100 items-top dark:bg-zinc-900 sm:pt-0 sm:bg-center sm:bg-dotted-spacing-6 sm:dark:bg-dotted-zinc-800 sm:bg-dotted-zinc-300">
-        <div class="max-w-xl mx-auto sm:px-6 lg:px-8">
+        <div class="max-w-xl mx-auto">
             <div class="max-w-screen-xl px-4 py-8 mx-auto lg:py-16 lg:px-6">
-                <div class="max-w-screen-sm mx-auto text-center">
+                <div class="text-center">
+                    <x-logo />
+
                     <h1 class="mb-4 font-extrabold tracking-tight text-indigo-500 text-9xl">
                         @yield('code')
                     </h1>
