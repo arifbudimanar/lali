933ed02ab1c51f8cc6ea93465edad86a9fb21572
Arif Budiman Arrosyid <arifbudimanarrosyid@gmail.com>
2023-10-17 01:50:45 +0700

    add message for error 404 and 503

--- resources/views/errors/minimal.blade.php
+++ resources/views/errors/minimal.blade.php
@@ -17,7 +17,7 @@
     <div
         class="relative flex items-center justify-center min-h-screen bg-zinc-100 items-top dark:bg-zinc-900 sm:pt-0 sm:bg-center sm:bg-dotted-spacing-6 sm:dark:bg-dotted-zinc-800 sm:bg-dotted-zinc-300">
         <div class="max-w-xl mx-auto">
-            <div class="max-w-screen-xl px-4 py-8 mx-auto lg:py-16 lg:px-6">
+            <div class="max-w-screen-xl mx-auto ">
                 <div class="text-center">
                     <x-logo />
 
@@ -29,9 +29,23 @@
                         @yield('message')
                     </p>
 
-                    <x-button-link href="{{ route('home') }}" wire:navigate>
-                        {{ __('Home') }}
-                    </x-button-link>
+                    @if ($exception->getStatusCode() == 404)
+                        <p class="mb-8 text-2xl font-bold tracking-tight text-zinc-700 dark:text-zinc-300">
+                            {{ __('Sorry, we can\'t find that page.') }}
+                        </p>
+                    @endif
+
+                    @if ($exception->getStatusCode() == 503)
+                        <p class="mb-8 text-2xl font-bold tracking-tight text-zinc-700 dark:text-zinc-300">
+                            {{ __('We are currently performing some maintenance. We will be back shortly.') }}
+                        </p>
+                    @endif
+
+                    @unless ($exception->getStatusCode() == 503)
+                        <x-button-link href="{{ route('home') }}" wire:navigate>
+                            {{ __('Home') }}
+                        </x-button-link>
+                    @endunless
                 </div>
             </div>
         </div>
