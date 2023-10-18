b2b37435412ce914ee573358011f4857e28ac5fd
Arif Budiman Arrosyid <arifbudimanarrosyid@gmail.com>
2023-10-19 03:14:49 +0700

    fix tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,6 @@ import defaultTheme from "tailwindcss/defaultTheme";
 export default {
     content: [
         "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
-        "./vendor/laravel/jetstream/**/*.blade.php",
         "./storage/framework/views/*.php",
         "./resources/views/**/*.blade.php",
     ],
