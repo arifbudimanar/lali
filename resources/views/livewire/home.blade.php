fde395848f5150e5438df3b351d8f0192cf52992
Arif Budiman Arrosyid <arifbudimanarrosyid@gmail.com>
2023-10-31 03:21:40 +0700

    wip

--- resources/views/livewire/home.blade.php
+++ resources/views/livewire/home.blade.php
@@ -6,6 +6,12 @@
     </x-slot>
 
     <div class="py-2 space-y-2 sm:py-8 sm:space-y-8">
-        <x-app-card width="full" title="{{ __('Welcome to :appname', ['appname' => config('app.name')]) }}" />
+        <x-app-card width="full" title="{{ __('Welcome to :appname', ['appname' => config('app.name')]) }}">
+            @auth
+                <x-slot name="description">
+                    {{ __('You are logged in as :name.', ['name' => auth()->user()->name]) }}
+                </x-slot>
+            @endauth
+        </x-app-card>
     </div>
 </div>
