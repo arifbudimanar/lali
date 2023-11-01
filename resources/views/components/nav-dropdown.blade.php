da33845362ef10f90ee377863d604e0d2b2ffe75
Arif Budiman Arrosyid <arifbudimanarrosyid@gmail.com>
2023-11-01 09:58:27 +0700

    nav dropdown arrow icon with component

--- resources/views/components/nav-dropdown.blade.php
+++ resources/views/components/nav-dropdown.blade.php
@@ -12,14 +12,8 @@ just nav-link or responsive nav-link --}}
     <button {{ $attributes->merge(['class' => $classes]) }} @click="open=!open">
         {{ $name }}
         <span>
-            <svg xmlns="http://www.w3.org/2000/svg" x-show="!open" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                stroke="currentColor" class="w-4 h-4">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
-            </svg>
-            <svg xmlns="http://www.w3.org/2000/svg" x-show="open" style="display: none;" fill="none"
-                viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 15.75l7.5-7.5 7.5 7.5" />
-            </svg>
+            <x-icon-arrow-down x-show="!open" class="w-4 h-4" />
+            <x-icon-arrow-up x-show="open" style="display: none;" class="w-4 h-4" />
         </span>
     </button>
 
