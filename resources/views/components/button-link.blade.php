6fb3897cda560c0e214f44a7625954a69be48475
Arif Budiman Arrosyid <arifbudimanarrosyid@gmail.com>
2023-11-01 08:12:47 +0700

    wip

--- resources/views/components/button-link.blade.php
+++ resources/views/components/button-link.blade.php
@@ -1,7 +1,6 @@
 <a
     {{ $attributes->merge([
-        'class' =>
-            'inline-block rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600',
+        'class' => 'inline-block rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white hover:bg-indigo-500',
     ]) }}>
     {{ $slot }}
 </a>
