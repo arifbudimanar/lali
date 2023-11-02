b4c1aa9f74d71c91f26cfecbc58deec83ae888b4
Arif Budiman Arrosyid <arifbudimanarrosyid@gmail.com>
2023-11-03 02:59:37 +0700

    refactor template

--- resources/views/template/app.blade.php
+++ resources/views/template/app.blade.php
@@ -1,43 +1,84 @@
+{{--
+No need to add #[Layout('layouts.app')] to the top of
+the function render() in the Livewire component.
+Because the default layout is #[Layout('layouts.app')].
+--}}
+
 <div>
-    {{-- Optional --}}
     <x-slot name="header">
         <h2 class="text-xl font-semibold leading-tight text-zinc-800 dark:text-zinc-200">
             {{ __('App Header Title') }}
         </h2>
     </x-slot>
 
-    {{-- Optional --}}
     <x-slot name="actions">
-        {{-- You can use button link component or other component --}}
         <x-button-link href="#">
             {{ __('Header Actions') }}
         </x-button-link>
     </x-slot>
 
     <div class="py-2 space-y-2 sm:py-8 sm:space-y-8">
-        <x-app-card>
-            {{-- Optional --}}
+        <x-app-card maxWidth="7xl" title="{{ __('Card Title') }}" description="{{ __('Card Description.') }}">
             <x-slot name="actions">
-                {{-- You can use button link component or other component --}}
                 <x-button-link href="#">
                     {{ __('Card Actions') }}
                 </x-button-link>
             </x-slot>
 
-            {{-- Required --}}
-            <x-slot name="title">
-                {{ __('Card Title') }}
-            </x-slot>
-
-            {{-- Optional --}}
-            <x-slot name="description">
-                {{ __('Card Description.') }}
-            </x-slot>
-
-            {{-- Optional --}}
             <x-slot name="content">
                 {{ __('Card Content') }}
             </x-slot>
         </x-app-card>
+
+        <x-app-card maxWidth="7xl">
+            <x-slot name="actions">
+                <x-button-link href="#">
+                    {{ __('Card Actions') }}
+                </x-button-link>
+            </x-slot>
+
+            <x-slot name="title">
+                {{ __('Card Title') }}
+            </x-slot>
+
+            <x-slot name="description">
+                {{ __('Card Description.') }}
+            </x-slot>
+
+            <x-slot name="content">
+                {{ __('Card Content') }}
+            </x-slot>
+        </x-app-card>
+
+        <x-form-card submit="submitFunction" maxWidth="7xl" title="{{ __('Card Title') }}"
+            description="{{ __('Card Description.') }}">
+            <x-slot name="form">
+                {{-- Name --}}
+                <div class="col-span-6 sm:col-span-4">
+                    <x-label for="name" value="{{ __('Name') }}" />
+                    <x-text-input id="name" type="text" class="block w-full mt-1" wire:model="name" required
+                        autofocus autocomplete="name" placeholder="{{ __('Full Name') }}" />
+                    <x-input-error for="name" class="mt-2" />
+                </div>
+
+                {{-- Email --}}
+                <div class="col-span-6 sm:col-span-4">
+                    <x-label for="email" value="{{ __('Email') }}" />
+                    <x-text-input id="email" type="text" class="block w-full mt-1" wire:model="email" required
+                        autocomplete="email" placeholder="{{ __('example@mail.com') }}" />
+                    <x-input-error for="email" class="mt-2" />
+                </div>
+            </x-slot>
+
+            <x-slot name="actions">
+                <x-button wire:loading.attr="disabled">
+                    {{ __('Submit') }}
+                </x-button>
+
+                <x-secondary-button wire:click="cancelFunction" wire:loading.attr="disabled">
+                    {{ __('Cancel') }}
+                </x-secondary-button>
+            </x-slot>
+        </x-form-card>
     </div>
 </div>
