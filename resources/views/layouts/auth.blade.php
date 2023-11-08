09c4224185338aeaaa242b601251a474f18de3b4
Arif Budiman Arrosyid <arifbudimanarrosyid@gmail.com>
2023-11-09 05:49:27 +0700

    Update auth layout with new background and text colors

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -17,12 +17,12 @@
     @livewireStyles
 </head>
 
-<body class="bg-zinc-100 dark:bg-zinc-900">
+<body class="bg-zinc-100 dark:bg-zinc-900 text-zinc-900 dark:text-zinc-100">
     <x-toaster-hub />
 
     {{-- Page Content --}}
     <div
-        class="min-w-full min-h-screen font-sans antialiased text-zinc-900 bg-zinc-100 sm:bg-center dark:text-zinc-100 dark:bg-zinc-900 sm:bg-dotted-spacing-6 sm:dark:bg-dotted-zinc-800 sm:bg-dotted-zinc-300">
+        class="min-w-full min-h-screen font-sans antialiased sm:bg-center sm:bg-dotted-spacing-6 sm:dark:bg-dotted-zinc-800 sm:bg-dotted-zinc-300">
         {{ $slot }}
     </div>
 
