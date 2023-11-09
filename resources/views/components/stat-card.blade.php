84062bdd4bb3e7409cca092ce634b4f2ec31285c
Arif Budiman Arrosyid <arifbudimanarrosyid@gmail.com>
2023-11-09 08:51:01 +0700

    Update stat-card.blade.php with new styles and layout

--- resources/views/components/stat-card.blade.php
+++ resources/views/components/stat-card.blade.php
@@ -1,7 +1,11 @@
 @props(['title', 'stats'])
 
-<div class="flex flex-col justify-between w-full p-5 rounded-lg bg-zinc-100 dark:bg-zinc-900">
-    <h3 class="text-base font-medium text-zinc-700 dark:text-zinc-300">{{ $title }}</h3>
+<div class="flex flex-col justify-between w-full gap-1 p-4 rounded-lg sm:p-6 sm:gap-2 bg-zinc-100 dark:bg-zinc-900">
+    <h3 class="text-base font-medium text-zinc-700 dark:text-zinc-300">
+        {{ $title }}
+    </h3>
 
-    <p class="mt-1 text-2xl font-bold sm:mt-2 text-zinc-900 dark:text-zinc-100">{{ $stats }}</p>
+    <p class="text-2xl font-bold text-indigo-600 dark:text-indigo-400">
+        {{ $stats }}
+    </p>
 </div>
