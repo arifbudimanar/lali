3a4468601dfad0cae57fc935b282421c0eb3f9cd
Arif Budiman Arrosyid <arifbudimanarrosyid@gmail.com>
2023-11-10 08:13:28 +0700

    Update favicon and add manifest file

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -5,6 +5,10 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('apple-touch-icon.png') }}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('favicon-32x32.png') }}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('favicon-16x16.png') }}">
+    <link rel="manifest" href="{{ asset('site.webmanifest') }}">
 
     <title>
         {{ config('app.name', 'Laravel') }}{{ Route::currentRouteName() ? ' - ' . __(ucwords(str_replace('.', ' ', Route::currentRouteName()))) : '' }}
