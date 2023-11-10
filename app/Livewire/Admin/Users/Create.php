6ae92adbff2eeaa3492e7b27656805468accad95
Arif Budiman Arrosyid <arifbudimanarrosyid@gmail.com>
2023-11-10 09:06:04 +0700

    Import Illuminate\Support\Facades\Hash to fix Hash class not found error in Create.php

--- app/Livewire/Admin/Users/Create.php
+++ app/Livewire/Admin/Users/Create.php
@@ -3,7 +3,7 @@
 namespace App\Livewire\Admin\Users;
 
 use App\Models\User;
-use Hash;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\View\View;
 use Livewire\Attributes\Layout;
 use Livewire\Component;
