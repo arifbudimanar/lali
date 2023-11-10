489eb2918614c06477b1139331dd82bfb62d9811
Arif Budiman Arrosyid <arifbudimanarrosyid@gmail.com>
2023-11-10 09:53:16 +0700

    Update Laravel version to 10.31.0 in _ide_helper.php

--- _ide_helper.php
+++ _ide_helper.php
@@ -4,7 +4,7 @@
 
 /**
  * A helper file for Laravel, to provide autocomplete information to your IDE
- * Generated for Laravel 10.30.1.
+ * Generated for Laravel 10.31.0.
  *
  * This file should not be included in your code, only analyzed by your IDE!
  *
