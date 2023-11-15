900c13f6e2cb4700b6dd4275dfdfe0cf6a4535bb
Arif Budiman Arrosyid <arifbudimanarrosyid@gmail.com>
2023-11-15 10:28:30 +0700

    Increase number of seeded users.

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -12,14 +12,14 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        for ($i = 0; $i < 200; $i++) {
+        for ($i = 0; $i < 1000; $i++) {
             User::factory()->create([
                 'created_at' => fake()->dateTimeBetween('-3 month', '-1 week'),
                 'updated_at' => fake()->dateTimeBetween('-1 week', 'now'),
             ]);
         }
 
-        for ($i = 0; $i < 50; $i++) {
+        for ($i = 0; $i < 200; $i++) {
             User::factory()->unverified()->create([
                 'created_at' => fake()->dateTimeBetween('-3 month', '-1 week'),
                 'updated_at' => fake()->dateTimeBetween('-1 week', 'now'),
