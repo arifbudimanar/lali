ff634009ba8fc642e7d5f1668d5dab058b8c4cff
Arif Budiman Arrosyid <arifbudimanarrosyid@gmail.com>
2023-11-15 10:00:51 +0700

    Refactor table-body-not-found component to use custom icon

--- resources/views/components/table-body-not-found.blade.php
+++ resources/views/components/table-body-not-found.blade.php
@@ -2,13 +2,7 @@
     'class' => 'px-6 py-4 space-x-2 whitespace-nowrap',
 ]) }}>
     <div class="inline-flex items-center font-medium whitespace-nowrap">
-        <span>
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                stroke="currentColor" class="w-5 h-5 mr-2">
-                <path stroke-linecap="round" stroke-linejoin="round"
-                    d="M20.25 7.5l-.625 10.632a2.25 2.25 0 01-2.247 2.118H6.622a2.25 2.25 0 01-2.247-2.118L3.75 7.5m6 4.125l2.25 2.25m0 0l2.25 2.25M12 13.875l2.25-2.25M12 13.875l-2.25 2.25M3.375 7.5h17.25c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125z" />
-            </svg>
-        </span>
+        <x-icon-data-not-found class="w-5 h-5 mr-2" />
 
         {{ __('Data Not Found') }}
     </div>
