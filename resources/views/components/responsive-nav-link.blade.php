be240db6cf2fc9f37434c3b5d713db1f31cbfe12
Arif Budiman Arrosyid <arifbudimanarrosyid@gmail.com>
2023-11-24 09:23:36 +0700

    Fix styling classes in nav components

--- resources/views/components/responsive-nav-link.blade.php
+++ resources/views/components/responsive-nav-link.blade.php
@@ -1,7 +1,7 @@
 @props(['active'])
 
 @php
-    $classes = $active ?? false ? 'block px-3 py-2 text-sm font-medium text-zinc-700 dark:text-white bg-zinc-100 dark:bg-zinc-900 rounded-md' : 'block px-3 py-2 text-sm font-medium text-zinc-500 dark:text-zinc-300 rounded-md hover:bg-zinc-100 dark:hover:bg-zinc-900 hover:text-zinc-700 dark:hover:text-white';
+    $classes = $active ?? false ? 'block px-3 py-2 text-sm font-medium text-zinc-700 dark:text-zinc-100 bg-zinc-100 dark:bg-zinc-900 rounded-md' : 'block px-3 py-2 text-sm font-medium text-zinc-700 dark:text-zinc-100 rounded-md hover:bg-zinc-100 dark:hover:bg-zinc-900 hover:text-zinc-700 dark:hover:text-white';
 @endphp
 
 <a {{ $attributes->merge(['class' => $classes]) }}>
